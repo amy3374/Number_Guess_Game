bb3e8912840ae8e7cce8d0737241cda533a595b5
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-04-01 00:35:36 +0900

    number guess game

--- main.js
+++ main.js
@@ -4,7 +4,6 @@
 // 랜덤번호가 < 유저번호, Down!!
 // 랜덤번호가 > 유저번호, Up!!
 // Reset버튼 누르면 게임이 리셋
-//
 // 5번의 기회를 다쓰면 게임이 끝난다(play 버튼이 disable 된다)
 // 유저가 1~100 범위 밖에 숫자를 입력하면 알려주고, 기회를 깍지 않는다
 // 유저가 이미 입력한 숫자를 입력하면 알려주고, 기회를 깍지 않는다.
@@ -16,6 +15,7 @@ let resultText = document.querySelector(".result-text");
 let chanceArea = document.getElementById("chance-area");
 let chance = 5;
 let gameOver = false;
+let history = [];
 
 pickRandomNum = () => {
   randomNum = Math.floor(Math.random() * 100 + 1);
@@ -25,6 +25,18 @@ pickRandomNum = () => {
 play = () => {
   userValue = input.value;
 
+  if (userValue < 1 || userValue > 100) {
+    resultText.textContent = "1~100사이의 값을 입력해주세요";
+    return;
+  }
+  if (history.includes(userValue)) {
+    resultText.textContent = "이미 입력한 값입니다. 다시 입력해주세요";
+    return;
+  }
+
+  chance--;
+  chanceArea.textContent = `남은 기회는 ${chance}번`;
+
   if (userValue < randomNum) {
     resultText.textContent = "UP!";
   } else if (userValue > randomNum) {
@@ -33,13 +45,8 @@ play = () => {
     resultText.textContent = "맞췄습니다!!";
     gameOver = true;
   }
-  if (userValue < 1 || userValue > 100) {
-    resultText.textContent = "1~100사이의 값을 입력해주세요";
-    return;
-  }
 
-  chance -= 1;
-  chanceArea.textContent = `남은 기회는 ${chance}번`;
+  history.push(userValue);
 
   if (chance < 1) {
     gameOver = true;
@@ -57,6 +64,7 @@ reset = () => {
   chance = 5;
   chanceArea.textContent = "남은 기회는 5번";
   resultText.textContent = "죽기 싫으면 맞춰라";
+  history = [];
 };
 
 playButton.addEventListener("click", play);
